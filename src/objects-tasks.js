093a7ba629177e223ba4baafe8197b571fa9d8e9
Ilya Spirkin <spirkinilya@gmail.com>
2024-02-28 22:09:09 +0300

    feat: removeProperties

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -51,8 +51,12 @@ function mergeObjects(/* objects */) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, 'age') => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const copyObj = shallowCopy(obj);
+  keys.forEach((key) => {
+    delete copyObj[key];
+  });
+  return copyObj;
 }
 
 /**
