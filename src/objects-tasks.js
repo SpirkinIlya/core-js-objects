7e149be938cc23109efdf7ab438f1262d9ab3490
Ilya Spirkin <spirkinilya@gmail.com>
2024-02-29 20:11:07 +0300

    feat: fromJSON

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -210,8 +210,10 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const obj1 = JSON.parse(json);
+  Object.setPrototypeOf(obj1, proto);
+  return obj1;
 }
 
 /**
